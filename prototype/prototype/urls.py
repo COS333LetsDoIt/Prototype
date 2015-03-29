75f1883032d0b22122ccda1a112d1eb2377c9516
leejieliang <jiell@princeton.edu>
2015-03-29 17:44:54 -0400

    Placeholder index html page

--- prototype/prototype/urls.py
+++ prototype/prototype/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'prototype.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^prototypeApp/', include('prototypeApp.urls',  namespace='polls')),
     url(r'^admin/', include(admin.site.urls)),
 )
