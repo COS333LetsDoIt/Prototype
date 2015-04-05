1bfa058fb2f48cf379058d0e42dd120eb18cb50f
leejieliang <jiell@princeton.edu>
2015-04-05 12:01:31 -0400

    CSS now works with django

--- prototype/prototypeApp/urls.py
+++ prototype/prototypeApp/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import patterns, include, url
 from prototypeApp import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+# urlpatterns = patterns('',
+#     # Examples:
+#     # url(r'^$', 'prototype.views.home', name='home'),
+#     # url(r'^blog/', include('blog.urls')),
+
+#     url(r'^$', views.index, name='index'),
+# )
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'prototype.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^$', views.index, name='index'),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
