c02a8cddd196702b5df40538d1f88fc7640c59b1
Eric Scheider <ericds@princeton.edu>
2015-04-05 12:53:48 -0400

    added events

--- prototype/prototypeApp/views.py
+++ prototype/prototypeApp/views.py
@@ -3,5 +3,6 @@ from prototypeApp.models import Person, Group, Event
 
 # Create your views here.
 def index(request):
-	context = {}
-	return render(request, 'prototypeApp/index.html',context)
+	event_list = Event.objects.order_by('starttime')
+	context = {"event_list": event_list}
+	return render(request, 'prototypeApp/index.html', context)
