06db7c89fb85cae53510b58776706fb43fbd4aac
Eric Scheider <ericds@princeton.edu>
2015-04-10 17:18:08 -0400

    Fixed date time form so that it actually works (badly)

--- prototype/prototypeApp/views.py
+++ prototype/prototypeApp/views.py
@@ -1,8 +1,51 @@
 from django.shortcuts import render
 from prototypeApp.models import Person, Group, Event
+from django import forms
+from django.db import models
+from django.forms import ModelForm
+
+#from datetimewidget.widgets import DateTimeWidget
+import datetime
+from django.http import HttpResponseRedirect
+
+
+class EventForm(ModelForm):
+    class Meta:
+        model = Event
+        fields = ['name','starttime','endtime']
+
+
+#starttime
+#endtime
+#friends
+#groups
+
+# taken from https://docs.djangoproject.com/en/1.8/topics/forms/#forms-in-django
+def get_event_form(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = EventForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            new_event = form.save()
+    # if a GET (or any other method) we'll create a blank form
+    
+    form = EventForm(initial={'starttime': datetime.datetime.now(), 'endtime': datetime.datetime.now()})
+
+    return form
 
 # Create your views here.
 def index(request):
-	event_list = Event.objects.order_by('starttime')
-	context = {"event_list": event_list}
-	return render(request, 'prototypeApp/index.html', context)
+    event_list = Event.objects.order_by('starttime')
+    event_form = get_event_form(request)
+    context = {"event_list": event_list, 'form': event_form}
+    return render(request, 'prototypeApp/index.html', context)
+
+def signup(request):
+    event_list = Event.objects.order_by('starttime')
+    context = {"event_list": event_list}
+    return render(request, 'prototypeApp/index.html', context)
