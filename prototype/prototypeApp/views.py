6c4fb0d05db6a5013a9ebb59bf9aed20d99d761b
leejieliang <jiell@princeton.edu>
2015-04-27 21:48:37 -0400

    index page changes to add event form

--- prototype/prototypeApp/views.py
+++ prototype/prototypeApp/views.py
@@ -50,9 +50,9 @@ def get_event_form(request):
             for friend_name in request.POST.get("friends", '').split(', '):
                 friends = Person.objects.filter(name=friend_name)
                 #print "found friend"
-                new_event.pendingMembers.add(request.user.person)
+                new_event.members.add(request.user.person)
                 if friends.exists():
-                    new_event.pendingMembers.add(friends[0])
+                    new_event.members.add(friends[0])
                     #friends[0].event_set.add(new_event)
                     #print "added friend to event"
     # if a GET (or any other method) we'll create a blank form
