16a07a89e782af226d46889b81d4661ccbdc9d1e
sjayanti@princeton.edu <sjayanti@princeton.edu>
2015-05-04 11:22:11 -0400

    whitenoise installed and added to settings etc...

--- prototype/wsgi.py
+++ prototype/wsgi.py
@@ -8,7 +8,14 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prototype.settings")
 
-from django.core.wsgi import get_wsgi_application
+#from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
+#application = get_wsgi_application()
