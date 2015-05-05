25cdd18993f6437f286464f5309324693f455629
scherzono2 <tzheng@princeton.edu>
2015-05-05 13:28:30 -0400

    l

--- prototype/settings.py
+++ prototype/settings.py
@@ -76,10 +76,12 @@ WSGI_APPLICATION = 'prototype.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
 
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.path.join(BASE_DIR, 'nameOfOurDatabase'),
+        'HOST' : '127.0.0.1',
+        'PORT': '',
     }
 }
 
